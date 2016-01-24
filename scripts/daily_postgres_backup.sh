824708ab4fea689a3b754bdbe880eee6a3bd6990
Paul Corcoran <prcorcoran99@gmail.com>
2016-01-24 13:37:38 -0500

    use envdir to set S3 env variables for cron jobs

--- scripts/daily_postgres_backup.sh
+++ scripts/daily_postgres_backup.sh
@@ -2,5 +2,5 @@
 echo "*** the daily database backup script is running "
 #exec su postgres -c "/usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf"
 NOW=$(date +"%Y%m%d_%H%M%S")
-cd /tmp;/usr/bin/pg_dump -U postgres sbgc_development | gzip > sbgc_db_backup.$NOW.sql.gz
-/usr/local/bin/aws s3 mv sbgc_db_backup.$NOW.sql.gz s3://sbgc/$1/
+cd /tmp;/usr/bin/pg_dump -U postgres sbgc_production | gzip > sbgc_db_backup.$NOW.sql.gz
+envdir /etc/wal-e.d/env /usr/local/bin/aws s3 mv sbgc_db_backup.$NOW.sql.gz s3://sbgc/$1/
